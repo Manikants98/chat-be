a6eff410dd489573ec836fc99b091c3b0067772c
Mani Kant Sharma <dadzheromani@gmail.com>
2024-01-30 08:22:48 +0530

    mkx

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -5,14 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongo_config_1 = __importDefault(require("../config/mongo.config"));
-const user_1 = __importDefault(require("../schemas/user"));
+const user_1 = __importDefault(require("../models/user"));
 const router = express_1.default.Router();
 router.get('/', async (req, res) => {
+    res.send('MKX CHAT APIs');
+});
+router.get('/users', async (req, res) => {
     (0, mongo_config_1.default)();
     const users = await user_1.default.find();
     res.send({ users, message: 'User Get Successfully' });
-});
-router.get('/mani', (req, res) => {
-    res.send('Hello, Mani!');
 });
 exports.default = router;
