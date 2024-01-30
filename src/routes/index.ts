ef6ebe55630d5a659b352b96cf456a2e92ba4a08
Mani Kant Sharma <dadzheromani@gmail.com>
2024-01-30 08:12:05 +0530

    mkx

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,16 @@
 import express, { Request, Response } from 'express';
 import useConnectDB from '../config/mongo.config';
-import User from '../schemas/user';
+import User from '../models/user';
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
+  res.send('MKX CHAT APIs');
+});
+
+router.get('/users', async (req: Request, res: Response) => {
   useConnectDB();
   const users = await User.find();
   res.send({ users, message: 'User Get Successfully' });
-});
-
-router.get('/mani', (req: Request, res: Response) => {
-  res.send('Hello, Mani!');
 });
 
 export default router;
