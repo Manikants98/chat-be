e79d084f44e43fff61813d51fceb6eafe58fb538
Mani Kant Sharma <dadzheromani@gmail.com>
2024-02-07 23:53:21 +0530

    mkx

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,13 +6,15 @@ import { contactsFn } from '../controllers/contacts';
 import { messagesFn } from '../controllers/messages';
 
 const route = express.Router();
+
 route.get('/', (req: Request, res: Response) => {
   res.send('MKX CHAT APIs');
 });
-route.all('/users', getUsersFn);
-route.post('/signup', signUpFn);
-route.post('/signin', signInFn);
-route.all('/contacts', contactsFn);
-route.all('/messages', messagesFn);
+
+// route.all('/users', getUsersFn);
+// route.post('/signup', signUpFn);
+// route.post('/signin', signInFn);
+// route.all('/contacts', contactsFn);
+// route.all('/messages', messagesFn);
 
 export default route;
