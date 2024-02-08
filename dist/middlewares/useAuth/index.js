c0406f6ceefca978ca24de47f2bbde3984498600
Mani Kant Sharma <dadzheromani@gmail.com>
2024-02-08 22:31:26 +0530

    mkx

--- dist/middlewares/useAuth/index.js
+++ dist/middlewares/useAuth/index.js
@@ -1,10 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useAuth = void 0;
-const Users_1 = __importDefault(require("../../models/Users"));
+// import User from '../../models/Users';
 const useAuth = async (req, res, next) => {
     const withoutToken = ['/', '/upload', '/signup', '/signin'];
     if (withoutToken.includes(req.path)) {
@@ -14,10 +11,10 @@ const useAuth = async (req, res, next) => {
     if (!token) {
         return res.status(401).json({ message: 'Authorization is missing!' });
     }
-    const user = await Users_1.default.findOne({ token });
-    if (!user) {
-        return res.status(401).json({ message: 'Invalid Authorization Token' });
-    }
+    // const user = await User.findOne({ token });
+    // if (!user) {
+    //   return res.status(401).json({ message: 'Invalid Authorization Token' });
+    // }
     next();
 };
 exports.useAuth = useAuth;
