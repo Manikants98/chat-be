27f80855d630e8ceb9c36ed983920e3dde577c59
Mani Kant Sharma <dadzheromani@gmail.com>
2024-02-08 23:13:24 +0530

    mkx

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import routes from './routes';
 import useConnectDB from './config/mongo.config';
 import { useAuth } from './middlewares/useAuth';
-
+import cors from 'cors';
 const app = express();
 const PORT = 3001;
-
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(useConnectDB);
